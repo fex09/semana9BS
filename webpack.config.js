7094b5142b5ccfb3e7f7e820fc0efbb077485bd9
Fenix <fsolisg@gmail.com>
2018-04-04 21:07:55 -0600

    completar

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,31 @@ console.log(__dirname);
 module.exports = {
     entry: './src/index.js' ,
     output: {
-        path: __dirname + '/dist',
+        path: path.join(__dirname + '/dist'),
         filename: 'bundle.js'
+    },
+    module:{
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),       
+        port: 8000
     },
     plugins: [
        new CleanWebpackPlugin(['dist']),
        new HtmlWebpackPlugin({
-           template: './src'
+           template: './src/index.html'
        })
     ]
 
